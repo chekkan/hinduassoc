351e1449fd3d0878cf2e2f1276af011f6b0a3b39
Chekkan <harish_babu@outlook.com>
2014-03-09 10:43:48 +0000

    can now search for event title and order by event date and title

--- public/app/events/haEventListCtrl.js
+++ public/app/events/haEventListCtrl.js
@@ -1,3 +1,9 @@
 angular.module('app').controller('haEventListCtrl', function($scope, haEvent) {
     $scope.events = haEvent.query();
+
+    $scope.sortOptions = [
+        {value: "title", text: "Sort by Title"},
+        {value: "eventDate", text: "Sort by Event Date"}];
+
+    $scope.sortOrder = $scope.sortOptions[1].value;
 });
